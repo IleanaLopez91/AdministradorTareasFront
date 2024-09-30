ec70f654463cc06011faed30b75413ff0d40638c
Ileana <ferchu91garcia@gmail.com>
2024-09-30 14:54:54 -0300

    mostrando tareas y editandolas, falta formulario de edicion

--- src/components/tasks/TaskCard.tsx
+++ src/components/tasks/TaskCard.tsx
@@ -1,13 +1,22 @@
 import { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 import { Task } from "@/types/index";
+import { useNavigate } from "react-router-dom";
 
 type TaskCardProps = {
   task: Task;
 };
 
 function TaskCard({ task }: TaskCardProps) {
+  const navigate = useNavigate();
+
   return (
     <li className=" p-5 bg-white border border-slate-300 flex justify-between gap-3">
       <div className=" min-w-0 flex flex-col gap-y-4">
@@ -21,10 +30,10 @@ function TaskCard({ task }: TaskCardProps) {
       </div>
       <div className="flex shrink-0  gap-x-6">
         <Menu as="div" className="relative flex-none">
-          <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+          <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
             <span className="sr-only">opciones</span>
             <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
-          </Menu.Button>
+          </MenuButton>
           <Transition
             as={Fragment}
             enter="transition ease-out duration-100"
@@ -34,33 +43,36 @@ function TaskCard({ task }: TaskCardProps) {
             leaveFrom="transform opacity-100 scale-100"
             leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-              <Menu.Item>
+            <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+              <MenuItem>
                 <button
                   type="button"
                   className="block px-3 py-1 text-sm leading-6 text-gray-900"
                 >
                   Ver Tarea
                 </button>
-              </Menu.Item>
-              <Menu.Item>
+              </MenuItem>
+              <MenuItem>
                 <button
                   type="button"
                   className="block px-3 py-1 text-sm leading-6 text-gray-900"
+                  onClick={() =>
+                    navigate(location.pathname + `?editTask=${task._id}`)
+                  }
                 >
                   Editar Tarea
                 </button>
-              </Menu.Item>
+              </MenuItem>
 
-              <Menu.Item>
+              <MenuItem>
                 <button
                   type="button"
                   className="block px-3 py-1 text-sm leading-6 text-red-500"
                 >
                   Eliminar Tarea
                 </button>
-              </Menu.Item>
-            </Menu.Items>
+              </MenuItem>
+            </MenuItems>
           </Transition>
         </Menu>
       </div>
--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getProjectById } from "@/api/ProjectAPI";
 import AddTaskModal from "@/components/tasks/AddTaskModal";
 import TaskList from "@/components/tasks/TaskList";
+import EditTaskData from "@/components/tasks/EditTaskData";
 
 function ProjectDetailsView() {
   const navigate = useNavigate();
@@ -35,6 +36,7 @@ function ProjectDetailsView() {
         </nav>
         <TaskList tasks={data.tasks} />
         <AddTaskModal />
+        <EditTaskData />
       </>
     );
 }
