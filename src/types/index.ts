b01adba05ce4a2f6a27da0293a5228300eefa062
Ileana <ferchu91garcia@gmail.com>
2024-11-23 18:41:22 -0300

    creando formulario de login, falta funcionalidad

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,17 @@
 import { z } from "zod";
 
+/**Auth y Users */
+
+const authSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  password_confirmation: z.string(),
+});
+
+type Auth = z.infer<typeof authSchema>;
+export type UserLoginForm = Pick<Auth, "email" | "password">;
+
 //Task**
 
 export const taskStatusSchema = z.enum([
