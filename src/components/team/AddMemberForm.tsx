729cd220598eb6f94889263664d41d81ee120769
Ileana <ferchu91garcia@gmail.com>
2025-02-19 15:11:06 -0300

    buscando usuario para gregar al proyecto

--- src/components/team/AddMemberForm.tsx
+++ src/components/team/AddMemberForm.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import ErrorMessage from "../ErrorMessage";
 import { TeamMemberForm } from "@/types/index";
+import { findUserByEmail } from "@/api/TeamAPI";
+import SearchResult from "./SearchResult";
 
 export default function AddMemberForm() {
   const initialValues: TeamMemberForm = {
@@ -18,9 +20,14 @@ export default function AddMemberForm() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const mutation = useMutation({});
+  const mutation = useMutation({
+    mutationFn: findUserByEmail,
+  });
 
-  const handleSearchUser = async () => {};
+  const handleSearchUser = async (formData: TeamMemberForm) => {
+    const data = { projectId, formData };
+    mutation.mutate(data);
+  };
 
   return (
     <>
@@ -55,6 +62,13 @@ export default function AddMemberForm() {
           value="Buscar Usuario"
         />
       </form>
+      <div className=" mt-10">
+        {mutation.isPending && <p className=" text-center">Cargando...</p>}
+        {mutation.error && (
+          <p className=" text-center">{mutation.error.message}</p>
+        )}
+        {mutation.data && <SearchResult user={mutation.data} />}
+      </div>
     </>
   );
 }
